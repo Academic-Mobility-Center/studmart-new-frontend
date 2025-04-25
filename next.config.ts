4736f4e39149d1801aee9d2ea10d74f6b83ce263
vldpvnnk <vldpvnnk@bk.ru>
2025-04-25 11:36:51 +0700

    фикс конфига

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,20 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     output: "standalone",
+      images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "assets.example.com",
+        pathname: "/account123/**",
+      },
+      {
+        protocol: "https",
+        hostname: "picsum.photos",
+        pathname: "/**",
+      },
+    ],
+  },
     async redirects() {
         return [
             {
@@ -14,64 +28,3 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
-
-// import type { NextConfig } from "next";
-
-// const nextConfig: NextConfig = {
-//   output: "standalone",
-//   images: {
-//     remotePatterns: [
-//       {
-//         protocol: "https",
-//         hostname: "assets.example.com",
-//         pathname: "/account123/**",
-//       },
-//       {
-//         protocol: "https",
-//         hostname: "picsum.photos",
-//         pathname: "/**",
-//       },
-//     ],
-//   },
-//   async redirects() {
-//     return [
-//       {
-//         source: '/',
-//         destination: '/home',
-//         permanent: true,
-//       },
-//     ];
-//   },
-// };
-
-// export default nextConfig;
-// import type { NextConfig } from "next";
-
-// const nextConfig: NextConfig = {
-//   output: "standalone",
-//   images: {
-//     remotePatterns: [
-//       {
-//         protocol: "https",
-//         hostname: "assets.example.com",
-//         pathname: "/account123/**",
-//       },
-//       {
-//         protocol: "https",
-//         hostname: "picsum.photos",
-//         pathname: "/**",
-//       },
-//     ],
-//   },
-//   async redirects() {
-//     return [
-//       {
-//         source: '/',
-//         destination: '/home',
-//         permanent: true,
-//       },
-//     ];
-//   },
-// };
-
-// export default nextConfig;
