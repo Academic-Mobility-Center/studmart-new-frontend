e520915511c8df3534175c9565f056899497bb9b
Dmitriy Izotov <disaythis@gmail.com>
2025-06-12 16:33:44 +0300

    feat: адаптировал RegForm1

--- src/components/forms/reg-form-1/RegForm1.tsx
+++ src/components/forms/reg-form-1/RegForm1.tsx
@@ -2,9 +2,11 @@ import React, { useState } from 'react';
 
 import Link from 'next/link';
 
-import InputField from '@/components/fields/input/InputField';
-import PasswordField from '@/components/fields/password/PasswordField';
+import InputPasswordField from '@/components/ui/inputs/InputPasswordField';
+import InputTextField from '@/components/ui/inputs/InputTextField';
 import RegFormProps from '@/types/RegFormProps';
+
+import styles from './RegForm1.module.scss';
 
 const RegForm1: React.FC<RegFormProps> = ({ onClick, formData, handleChange }) => {
 	const [errors, setErrors] = useState<{
@@ -35,6 +37,7 @@ const RegForm1: React.FC<RegFormProps> = ({ onClick, formData, handleChange }) =
 		) {
 			newErrors.email = `Email должен содержать от ${minEmailLength} до ${maxEmailLength} символов`;
 		}
+
 		const password = formData?.password;
 		const isValidPassword = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z0-9]).{8,}$/.test(
 			password,
@@ -48,11 +51,13 @@ const RegForm1: React.FC<RegFormProps> = ({ onClick, formData, handleChange }) =
 			newErrors.password =
 				'Пароль должен состоять из заглавных и строчных букв, цифр и одного специального символа';
 		}
+
 		if (!formData.confirmPassword) {
 			newErrors.confirmPassword = 'Подтвердите пароль';
 		} else if (formData.password !== formData.confirmPassword) {
 			newErrors.confirmPassword = 'Пароли не совпадают';
 		}
+
 		setErrors(newErrors);
 		return Object.keys(newErrors).length === 0;
 	};
@@ -66,86 +71,53 @@ const RegForm1: React.FC<RegFormProps> = ({ onClick, formData, handleChange }) =
 	};
 
 	return (
-		<>
-			<form
-				onSubmit={handleSubmit}
-				className="border bg-[#f8f8f8] 
-            flex flex-col gap-5 w-full max-w-[410px] 
-            self-center p-[30px] rounded-[30px] 
-            border-solid border-[rgba(0,0,0,0.20)] mt-[25px]"
-			>
-				<div className="flex justify-between">
-					<h2 className="text-2xl font-extrabold text-[#032c28]">Регистрация</h2>
-					<h2 className="text-2xl font-extrabold text-[#032c28]">1/3</h2>
-				</div>
-				<div className="flex flex-col gap-1">
-					<InputField
-						label="Почта"
-						placeholder="Example@gmail.com"
-						onChange={handleChange}
-						name="email"
-						value={formData.email}
-						autoComplete="off"
-						width={350}
-						labelFontSize={14}
-						maxRows={1}
-					/>
-					{errors.email && <p className="text-red-600 text-sm font-medium mt-1">{errors.email}</p>}
-				</div>
-				<div className="flex flex-col gap-1">
-					<PasswordField
-						label="Пароль"
-						placeholder="********"
-						onChange={handleChange}
-						name="password"
-						value={formData.password}
-						labelFontSize={14}
-					/>
-					{errors.password && (
-						<p className="text-red-600 text-sm font-medium mt-1">{errors.password}</p>
-					)}
-				</div>
-				<div className="flex flex-col gap-1">
-					<PasswordField
-						label="Повторите пароль"
-						placeholder="********"
-						onChange={handleChange}
-						name="confirmPassword"
-						value={formData.confirmPassword}
-						labelFontSize={14}
-					/>
-					{errors.confirmPassword && (
-						<p className="text-red-600 text-sm font-medium mt-1">{errors.confirmPassword}</p>
-					)}
-				</div>
-
-				<InputField
-					label="Есть промокод?"
-					placeholder="Hhufs8a7auh40egij"
-					width={350}
-					name="promocode"
-					value={formData?.promocode}
-					labelFontSize={14}
-					maxRows={1}
-					onChange={handleChange}
-				/>
-
-				<div className="flex flex-col gap-4">
-					<button
-						type="submit"
-						className="bg-[#8fe248] text-[#032c28] p-2 text-sm font-bold uppercase rounded-2xl"
-					>
-						Далее
-					</button>
-					<p className="text-sm font-bold text-[#032c28] text-center">
-						Уже есть аккаунт?
-						<Link href="/login" className="text-[#6dbc29] underline pl-2">
-							Войти
-						</Link>
-					</p>
-				</div>
-			</form>
-		</>
+		<form onSubmit={handleSubmit} className={styles.form}>
+			<div className={styles.header}>
+				<h2 className={styles.title}>Регистрация</h2>
+				<h3 className={styles.title}>1/3</h3>
+			</div>
+			<InputTextField
+				label="Почта"
+				placeholder="Example@gmail.com"
+				autoComplete="off"
+				name="email"
+				onChange={handleChange}
+				value={formData.email}
+				errorText={errors.email}
+			/>
+			<InputPasswordField
+				name="password"
+				onChange={handleChange}
+				value={formData.password}
+				errorText={errors.password}
+			/>
+			<InputPasswordField
+				label="Повторите пароль"
+				name="confirmPassword"
+				onChange={handleChange}
+				value={formData.confirmPassword}
+				errorText={errors.confirmPassword}
+			/>
+			<InputTextField
+				label="Есть промокод?"
+				placeholder="Hhufs8a7auh40egij"
+				name="promocode"
+				autoComplete="off"
+				onChange={handleChange}
+				value={formData?.promocode}
+			/>
+			<div className={styles.actions}>
+				<button type="submit" className={styles['submit-button']}>
+					Далее
+				</button>
+				<p className={styles['login-link']}>
+					Уже есть аккаунт?
+					<Link href="/login" className={styles['login-anchor']}>
+						Войти
+					</Link>
+				</p>
+			</div>
+		</form>
 	);
 };
 
