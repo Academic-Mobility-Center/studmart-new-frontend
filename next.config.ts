dedfe9c2db92a50fa3e8e628a5c6c78a4c0cf089
Dmitriy Izotov <disaythis@gmail.com>
2025-06-23 13:04:47 +0300

    feature: добавил новый пакет чтобы svg воспринимались как компоненты

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,20 @@ const HOST = process.env.NEXT_PUBLIC_API_URL;
 
 const nextConfig: NextConfig = {
 	output: 'standalone',
+	webpack(config) {
+		const fileLoaderRule = config.module.rules.find((rule: any) => rule.test?.test?.('.svg'));
+		if (fileLoaderRule) {
+			fileLoaderRule.exclude = /\.svg$/i;
+		}
+
+		config.module.rules.push({
+			test: /\.svg$/i,
+			issuer: /\.[jt]sx?$/,
+			use: ['@svgr/webpack'],
+		});
+
+		return config;
+	},
 	images: {
 		remotePatterns: [
 			{
